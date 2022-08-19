541acb4e301671d85f49e76deb8939e3ae2c3952
kenko <kyoshitomi.cursor@gmail.com>
2022-08-20 08:13:30 +0900

    test

--- .circleci/merged.sh
+++ .circleci/merged.sh
@@ -9,7 +9,8 @@ COMMIT_MSG=$(git log --pretty=format:"%s" -n 1 $CIRCLE_SHA1)
 # COMMIT_MSG='Merge pull request #3 from xxxxx/feature/001-1'
 
 if [ $(echo $COMMIT_MSG | egrep '^Merge pull request #[0-9]+ from') ]; then
-  return 0
+  # return 0
+  echo "enter merge pull request"
 fi
 
-circleci step halt
+# circleci step halt
