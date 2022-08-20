c480ec8aae9fb358fc10d8d8c884001cee57a0ae
kenko <kyoshitomi.cursor@gmail.com>
2022-08-20 09:04:20 +0900

    use patern

--- .circleci/merged.sh
+++ .circleci/merged.sh
@@ -9,8 +9,9 @@ COMMIT_MSG=$(git log --pretty=format:"%s" -n 1 $CIRCLE_SHA1)
 # COMMIT_MSG='Merge pull request #3 from xxxxx/feature/001-1'
 
 if [ $(echo $COMMIT_MSG | egrep '^Merge pull request #[0-9]+ from') ]; then
-  return 0
-  # echo "enter merge pull request"
+  # return 0
+  echo "merge"
 fi
 
-circleci step halt
+# circleci step halt
+echo "commit"
