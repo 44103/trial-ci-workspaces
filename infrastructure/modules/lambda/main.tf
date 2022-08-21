ce8ade683c357310a5b37b8c03c5f3c343f0c0cf
kenko <kyoshitomi.cursor@gmail.com>
2022-08-22 06:13:13 +0900

    fix zip dir

--- infrastructure/modules/lambda/main.tf
+++ infrastructure/modules/lambda/main.tf
@@ -35,19 +35,19 @@ resource "aws_iam_role_policy" "_" {
 
 data "archive_file" "_" {
   type        = "zip"
-  source_dir  = "${local.func_dir}/target/x86_64-unknown-linux-musl/release/${var.name}"
-  output_path = "${local.func_dir}/target/x86_64-unknown-linux-musl/release/${var.name}/source.zip"
+  source_dir  = "${local.func_dir}/dist/${var.name}/bin"
+  output_path = "${local.func_dir}/dist/${var.name}/source.zip"
 
-  # depends_on = [
-  #   null_resource.place_dist
-  # ]
+  depends_on = [
+    null_resource.place_dist
+  ]
 }
 
 resource "aws_lambda_function" "_" {
   function_name    = local.name
   role             = aws_iam_role._.arn
   runtime          = "provided.al2"
-  handler          = "${var.name}.is.real.handler"
+  handler          = "bootstrap.is.real.handler"
   timeout          = 10
   filename         = data.archive_file._.output_path
   source_code_hash = data.archive_file._.output_base64sha256
@@ -57,20 +57,20 @@ resource "aws_lambda_function" "_" {
   }
 }
 
-# resource "null_resource" "place_dist" {
-#   triggers = {
-#     dist = filesha256("${local.func_dir}/target/x86_64-unknown-linux-musl/release/${var.name}")
-#   }
+resource "null_resource" "place_dist" {
+  triggers = {
+    dist = filesha256("${local.func_dir}/target/x86_64-unknown-linux-musl/release/${var.name}")
+  }
 
-#   provisioner "local-exec" {
-#     working_dir = local.func_dir
-#     interpreter = ["/bin/sh", "-c"]
-#     command     = <<-EOT
-#     mkdir -p ./dist/${var.name}
-#     cp ./target/x86_64-unknown-linux-musl/release/${var.name} ./dist/${var.name}/bootstrap
-#     EOT
-#   }
-# }
+  provisioner "local-exec" {
+    working_dir = local.func_dir
+    interpreter = ["/bin/sh", "-c"]
+    command     = <<-EOT
+    mkdir -p ./dist/${var.name}
+    cp ./target/x86_64-unknown-linux-musl/release/${var.name} ./dist/${var.name}/bin/bootstrap
+    EOT
+  }
+}
 
 resource "aws_cloudwatch_log_group" "_" {
   name = "/aws/lambda/${local.name}"
